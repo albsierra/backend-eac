2297b03d4576a8869616345a0e5cd91f69d4c684
Alberto Sierra <alberto.sierra@murciaeduca.es>
2026-03-11 16:54:33 +0100

    Tailwind y vistas

---
+++ resources/views/components/gradiente-badge.blade.php
@@ -0,0 +1,19 @@
+@props(['gradiente'])
+
+@php
+$clases = match($gradiente) {
+    'autonomo'    => 'badge-autonomo',
+    'supervisado' => 'badge-supervisado',
+    'guiado'      => 'badge-guiado',
+    'asistido'    => 'badge-asistido',
+    default       => 'badge-asistido',
+};
+$etiquetas = [
+    'autonomo'    => 'Autónomo',
+    'supervisado' => 'Supervisado',
+    'guiado'      => 'Guiado',
+    'asistido'    => 'Asistido',
+];
+@endphp
+
+<span class="{{ $clases }}">{{ $etiquetas[$gradiente] ?? $gradiente }}</span>
